28f67afaddaf8ec1739d5921f6db49c48214afaa
Samarth Patil <102871567+samarthpatilML@users.noreply.github.com>
2026-03-29 05:51:38 -0700

    new update

--- flaskey_setdev/cli.py
+++ flaskey_setdev/cli.py
@@ -2,7 +2,8 @@ import argparse
 from .generator import create_flask_project
 
 def main():
-    parser = argparse.ArgumentParser(description="Project Generator")
+    parser = argparse.ArgumentParser(description="Flaskey Project Generator")
+
     parser.add_argument("type", help="project type (flask)")
     parser.add_argument("name", help="project name")
 
--- flaskey_setdev/generator.py
+++ flaskey_setdev/generator.py
@@ -3,11 +3,11 @@ import os
 def create_flask_project(name):
     os.makedirs(name, exist_ok=True)
 
-    # folders
+    # create folders
     os.makedirs(f"{name}/templates", exist_ok=True)
     os.makedirs(f"{name}/static", exist_ok=True)
 
-    # app.py
+    # create app.py
     with open(f"{name}/app.py", "w") as f:
         f.write("""from flask import Flask
 
@@ -21,8 +21,8 @@ if __name__ == "__main__":
     app.run(debug=True)
 """)
 
-    # requirements.txt
+    # create requirements.txt
     with open(f"{name}/requirements.txt", "w") as f:
         f.write("flask\n")
 
-    print(f"🚀 Flask project '{name}' created!")
+    print(f"🚀 Flask project '{name}' created successfully!")
